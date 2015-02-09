9082e871ac06e7aa0f23409d436d2dbbbd9e8781
Rodrigo Luis Santander <roidersp@gmail.com>
2015-02-09 16:52:41 -0600

    dsfsdfds

--- js/indepth.js
+++ js/indepth.js
@@ -198,7 +198,7 @@ $(document).ready(function(){
 	 	$('.indepth_break').css("height",ventana_alto+"px");
 
 
-	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
+	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.30);
  	}
 	 //ventana_alto=ventana_alto-(ventana_alto*.15)
 	 	//$('.indepth_anuncio_section').css("height",ventana_alto-(ventana_alto*.10)+"px");
@@ -218,7 +218,7 @@ $(window).on("resize", function(){
     	$('#indepth_cover').css("height",(ventana_alto)+"px");
     	 if(ventana_alto>600){
 	 	$('.indepth_break').css("height",ventana_alto+"px");
-	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
+	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.30);
  	}
 	 if(navigator.platform == 'iPad'){
 	    //$("#indepth_parallax_back").css("background-size", "100%");
